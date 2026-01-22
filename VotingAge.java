ce1d8c4e83c38397e7c40c9abd4dfd22d833aef9
Gayatri <gayatrik@gmail.com>
2026-01-22 14:57:49 +0530

    Voting age

---
+++ VotingAge.java
@@ -0,0 +1,12 @@
+public class VotingAge {
+    public static void main(String[] args) {
+        int age=12;
+        if (age>=18){
+            System.out.println("You can Vote");
+        }
+        else{
+            System.out.println("Not eligible for voting");
+        }
+    }
+    
+}
