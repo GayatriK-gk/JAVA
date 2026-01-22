385a24ef40712c1b60f8a962a1a27222609a63ac
Gayatri <gayatrik@gmail.com>
2026-01-22 14:48:30 +0530

    first java basic program

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+    public static void main(String[] args) {
+        int num=2;
+        if(num%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
