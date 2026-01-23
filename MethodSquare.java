d01f6fbc4f7445789f4a38784018931a2a400e61
Gayatri <gayatrik@gmail.com>
2026-01-23 21:35:51 +0530

    Method Called

---
+++ MethodSquare.java
@@ -0,0 +1,9 @@
+public class MethodSquare {
+    public void area(){
+        System.out.println("Area of Square:"+(2*2));
+    }
+    public static void main(String args[]){
+        MethodSquare s=new MethodSquare();
+        s.area();
+    }
+}
