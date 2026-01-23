45fc22fe3f5e3fbb6abbaac76ce04c1773d479b1
Gayatri <gayatrik@gmail.com>
2026-01-23 22:22:39 +0530

    Student

---
+++ VariableStudent.java
@@ -0,0 +1,13 @@
+
+
+public class VariableStudent {
+    int roll=1;
+    String name="Oggy";
+    String dept="computer";
+    public static void main(String args[]){
+        VariableStudent s1=new VariableStudent();
+        System.out.println("Student Name:"+s1.name);
+        System.out.println("Roll no.:"+s1.roll);
+        System.out.println("Department:"+s1.dept);
+    }
+}
