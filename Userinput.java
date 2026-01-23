e27836bb016abbe085982f277a22e8633449b62a
Gayatri <gayatrik@gmail.com>
2026-01-23 20:00:50 +0530

    User input

---
+++ Userinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Userinput {
+    public static void main (String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name=s.nextLine();
+        System.out.println("Hieee!!!"+name);
+
+
+    }
+    
+}
