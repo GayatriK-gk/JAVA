a237b521f7ca6e5c8030746bb2495695f4f9fb44
Gayatri <gayatrik@gmail.com>
2026-01-23 21:09:38 +0530

    userinput

--- Userinput2.java
+++ Userinput2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Userinput2{
-    public ststic void main(String args[]){
+    public static void main(String args[]){
         Scanner s =new Scanner(System.in);
         System.out.println("Enter your Name:");
         String name=s.nextLine();
